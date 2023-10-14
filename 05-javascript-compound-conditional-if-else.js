5010f4a4340c79180852b18784b4fb0e58f0d280
Beregszászi István <beistvan@gmail.com>
2023-10-14 16:59:15 +0200

    JavaScript Compound Conditionals - If-Else

---
+++ 05-javascript-compound-conditional-if-else.js
@@ -0,0 +1,7 @@
+let a = 1234567; // 123456 - osztható 3-al; 1234567 - nem osztható 3-al
+
+if (a % 3 === 0) {
+  console.log("Osztható hárommal.");
+} else {
+  console.log("Nem osztható hárommal.");
+}
