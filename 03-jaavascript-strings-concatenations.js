9afaf0610eee32eae5ae33349c6cfdf1759df1a5
Beregszászi István <beistvan@gmail.com>
2023-10-14 16:55:48 +0200

    JavaScript Strings And String Concatenations

---
+++ 03-jaavascript-strings-concatenations.js
@@ -0,0 +1,13 @@
+let a = "First Text";
+
+let b = "Second Text";
+
+let c = a + " " + b;
+
+console.log(c); // "First Text Second Text"
+
+let keresztnev = "John";
+
+let vezeteknev = "Smith";
+
+console.log(vezeteknev + " " + keresztnev); // "Smith John"
